7f29e63ea5d6bbc65c9f12580cd1a8c0b7216977
smallwisp <4ubarov96@mail.ru>
2023-12-05 00:05:25 +0300

    added rendered table

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,15 @@ const financeForm = document.querySelector('.finance__form');
 const financeAmount = document.querySelector('.finance__amount');
 const financeReport = document.querySelector('.finance__report');
 const report = document.querySelector('.report');
-const reportClose = document.querySelector('.report__close');
+const reportOperationList = document.querySelector('.report__operation-list');
+const reportDates = document.querySelector('.report__dates');
+
+const API_URL = 'https://instinctive-ambitious-pick.glitch.me/api'
+
+const typesOperation = {
+  income: 'доход',
+  expenses: 'расход',
+} 
 
 let amount = 0
 
@@ -28,8 +36,21 @@ financeForm.addEventListener('submit', (event) => {
   }
 
   financeAmount.textContent = `${amount.toLocaleString()} ₽`
-
 })
+
+const getData = async(url) => {
+  try {
+    const response = await fetch(`${API_URL}${url}`)
+    if (!response.ok) {
+      throw new Error('HTTP error status:', response.status)
+    }
+
+    return await response.json()
+  } catch (error) {
+    console.error('Ошибка при получении данных:', error);
+    throw error
+  }
+};
 
 const closeReport = ({ target }) => {
   if (target.closest('.report__close') || (!target.closest('.report') && target !== financeReport)) {
@@ -40,8 +61,65 @@ const closeReport = ({ target }) => {
 
 const openReport = () => {
   report.classList.add('report__open')
-
   document.addEventListener('click', closeReport)
 };
 
-financeReport.addEventListener('click', openReport)
+const reformatDate = (dateStr) => {
+  const [year, month, day] = dateStr.split('-')
+  return `${day.padStart(2, '0')}.${month.padStart(2, '0')}.${year}`
+};
+
+const renderReport = (data) => {
+  reportOperationList.textContent = ''
+
+  const reportRows = data.map(({category, amount, description, date, type}) => {
+    const reportRow = document.createElement('tr')
+    reportRow.classList.add('report__row')
+
+    reportRow.innerHTML = `
+      <td class="report__cell">${category}</td>
+      <td class="report__cell" style="text-align: right">${amount.toLocaleString()}&nbsp;₽</td>
+      <td class="report__cell">${description}</td>
+      <td class="report__cell">${reformatDate(date)}</td>
+      <td class="report__cell">${typesOperation[type]}</td>
+      <td class="report__action-cell">
+        <button
+          class="report__button report__button_table">&#10006;</button>
+      </td>
+    `
+
+    return reportRow
+  })
+
+  reportOperationList.append(...reportRows)
+};
+
+financeReport.addEventListener('click', async () => {
+  openReport()
+  const data = await getData('/test')
+  renderReport(data)
+})
+
+reportDates.addEventListener('submit', async(event) => {
+  event.preventDefault()
+
+  const formData =  Object.fromEntries(new FormData(reportDates))
+
+  const searchParams = new URLSearchParams()
+
+  if (formData.startDate) {
+    searchParams.append('startDate', formData.startDate)
+  }
+
+  if (formData.endDate) {
+    searchParams.append('endDate', formData.endDate)
+  }
+
+  const queryString = searchParams.toString()
+  const url = queryString ? `/test?${queryString}` : '/test'
+
+  const data = await getData(url)
+  renderReport(data)
+
+})
+
